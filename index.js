c186273165971064dd146ac305a68277099e9ce6
roman <51534566+roman-thapa@users.noreply.github.com>
2023-10-03 12:41:06 +0530

    Add unshift function and log value as comment

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
 function LinkedList() {
-    let head = null;
-    
-    let Node = function(data) {
-      this.data = data;
-      this.next = null;
-    }
-    
-    this.add = function(data) {
-      let node = new Node(data)
-      if(head === null) {
-        head =node
-      } else {
-        let currentNode = head;
-        
-        while(currentNode.next) {
-          currentNode = currentNode.next
-        }
-        currentNode.next = node
-      }
-      return head;
-    }
-    
-    this.delete = function(data) {
-      let currentNode = head
-      let previousNode;
-      if (currentNode.data === data) {
-        head = currentNode.next
-      } else {
-          while(currentNode.data !== data) {
-            previousNode = currentNode
-            currentNode = currentNode.next
-          }
-          
-          previousNode.next = currentNode.next
-      }
-      return head;
-    }
+  let head = null;
+  
+  let Node = function(data) {
+    this.data = data;
+    this.next = null;
   }
   
-  let node = new LinkedList()
+  this.push = function(data) {
+    let node = new Node(data)
+    if(head === null) {
+      head =node
+    } else {
+      let currentNode = head;
+      
+      while(currentNode.next) {
+        currentNode = currentNode.next
+      }
+      currentNode.next = node
+    }
+    return head;
+  }
   
-  console.log(node.add("hi"))
-  console.log(node.add("hello"))
-  console.log(node.add(100))
-  console.log(node.delete('hello'))
-  
+  this.delete = function(data) {
+    let currentNode = head
+    let previousNode;
+    if (currentNode.data === data) {
+      head = currentNode.next
+    } else {
+        while(currentNode.data !== data) {
+          previousNode = currentNode
+          currentNode = currentNode.next
+          if(!currentNode) return "Data Not found"
+        }
+        previousNode.next = currentNode.next
+    }
+    return head;
+  }
+  this.unShift = function() {
+    let currentNode = head;
+    head = currentNode.next;
+    return head
+  }
+}
+
+let node = new LinkedList()
+
+console.log(node.push("hi")) //Node { data: 'hi', next: null }
+console.log(node.push("hello")) //Node { data: 'hi', next: Node { data: 'hello', next: null } }
+console.log(node.push(100)) 
+/**
+ * Node {
+    data: 'hi',
+    next: Node { data: 'hello', next: Node { data: 100, next: null } }
+  }
+*/
+console.log(node.unShift()) //Node { data: 'hello', next: Node { data: 100, next: null } }
+console.log(node.delete('100')) //Data Not found
+console.log(node.delete(100)) //Node { data: 'hello', next: null }
